5e0a4b0c6cb9efa931fe0b24626d47ec521549fd
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-19 09:48:55 +1100

    Read user input and send to echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use babilado_types::Event;
+use jsonl::Connection;
+use std::io::{self, BufReader};
+use std::net::TcpStream;
+
+fn main() -> anyhow::Result<()> {
+    let stdin = io::stdin();
+
+    let stream = TcpStream::connect("127.0.0.1:9999")?;
+    let mut server_connection = Connection::new(BufReader::new(stream.try_clone()?), stream);
+
+    loop {
+        let mut message = String::new();
+        stdin.read_line(&mut message)?;
+
+        server_connection.write(&Event::NewMessage { text: message })?;
+        server_connection.flush()?;
+
+        let response: Event = server_connection.read()?;
+        dbg!(response);
+    }
 }
